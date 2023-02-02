e3e143b884d64d85d23b1cdde3d18f4ac2ed20f5
Marllon Menezes <marllon_dallas@hotmail.com>
2023-02-02 17:06:13 -0300

    finalização do projeto testes automaziados de api

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000/',
+    baseUrl: "http://localhost:3000/",
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: false,
+      json: true,
     },
-  
+  },
 });
